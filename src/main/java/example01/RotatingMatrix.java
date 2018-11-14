7f0c3c7021dd42e6ca14607d144ca16ff92fd058
David <xodnjs2549@naver.com>
2018-11-14 12:32:39 +0900

    add rotateNTimes

--- src/main/java/example01/RotatingMatrix.java
+++ src/main/java/example01/RotatingMatrix.java
@@ -16,4 +16,40 @@ public class RotatingMatrix {
     public String printResult() {
         return Arrays.stream(numbers).flatMapToInt(Arrays::stream).mapToObj(Integer::toString).collect(Collectors.joining(", "));
     }
+
+    public void rotate() {
+        int sideLength = numbers[0].length;
+        int first = numbers[0][0];
+
+        // left side
+        for (int j = 0; j < sideLength - 1; j++) {
+            numbers[j][0] = numbers[j + 1][0];
+        }
+
+        // lower side
+        for (int j = 0; j < sideLength - 1; j++) {
+            numbers[sideLength - 1][j] = numbers[sideLength - 1][j + 1];
+        }
+
+        // right side
+        for (int i = sideLength - 1; i > 0; i--) {
+            numbers[i][sideLength - 1] = numbers[i - 1][sideLength - 1];
+        }
+
+        // upper side
+        for (int j = sideLength - 1; j > 0; j--) {
+            if (j == 1) {
+                numbers[0][j] = first;
+            } else {
+                numbers[0][j] = numbers[0][j - 1];
+            }
+        }
+    }
+
+
+    public void rotateNTimes() {
+        for (int count = 0; count < rotatingCount; count++) {
+            rotate();
+        }
+    }
 }
--- src/test/java/example01/RotatingMatrixTest.java
+++ src/test/java/example01/RotatingMatrixTest.java
@@ -16,6 +16,9 @@ public class RotatingMatrixTest {
 
     private final int rotatingCount = 2;
 
+    private String defaultResult = "1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16";
+
+
     @Test
     public void create() {
         RotatingMatrix matrix = new RotatingMatrix(new UserInput(numbers, rotatingCount));
@@ -23,13 +26,24 @@ public class RotatingMatrixTest {
 
     @Test
     public void printResult() {
-        String defaultResult = "1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16";
         RotatingMatrix matrix = new RotatingMatrix(new UserInput(numbers, rotatingCount));
         assertThat(matrix.printResult(), is(defaultResult));
     }
 
+    // TODO Ignore for private method
     @Test
     public void rotate() {
+        String resultByRotate1Times = "5, 1, 2, 3, 9, 6, 7, 4, 13, 10, 11, 8, 14, 15, 16, 12";
+        RotatingMatrix matrix = new RotatingMatrix(new UserInput(numbers, rotatingCount));
+        matrix.rotate();
+        assertThat(matrix.printResult(), is(resultByRotate1Times));
+    }
 
+    @Test
+    public void rotateNTimes() {
+        String resultByRotate2Times = "9, 5, 1, 2, 13, 6, 7, 3, 14, 10, 11, 4, 15, 16, 12, 8";
+        RotatingMatrix matrix = new RotatingMatrix(new UserInput(numbers, rotatingCount));
+        matrix.rotateNTimes();
+        assertThat(matrix.printResult(), is(resultByRotate2Times));
     }
 }
