c7dd1ab1b1f665a5e791e195c442319b531a6954
David <xodnjs2549@naver.com>
2018-11-16 14:12:25 +0900

    add exception test

--- src/main/java/example02/StringUtils.java
+++ src/main/java/example02/StringUtils.java
@@ -10,8 +10,17 @@ public class StringUtils {
 
         HashMap<Integer, Integer> numberFrequencyChart = new HashMap<>();
         String[] numbers = userInput.split(", ");
+        if (numbers.length < 1 || numbers.length > 99) {
+            throw new IllegalArgumentException("에러 : 1개이상 99개이하의 자연수를 입력해주세요");
+        }
         for (String num : numbers) {
-            numberFrequencyChart.merge(Integer.parseInt(num), defaultValue, (existingValue, newValue) -> newValue = existingValue + increaseCount);
+            Integer number = null;
+            try {
+                number = Integer.parseInt(num);
+            } catch (NumberFormatException e) {
+                throw new NumberFormatException("에러 : 숫자만 입력 가능합니다");
+            }
+            numberFrequencyChart.merge(number, defaultValue, (existingValue, newValue) -> newValue = existingValue + increaseCount);
         }
         return numberFrequencyChart;
     }
--- src/test/java/example02/StringUtilsTest.java
+++ src/test/java/example02/StringUtilsTest.java
@@ -2,9 +2,9 @@ package example02;
 
 import org.junit.Test;
 
-import java.net.Inet4Address;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.StringJoiner;
 
 import static org.junit.Assert.assertNotNull;
 
@@ -15,6 +15,29 @@ public class StringUtilsTest {
         String userInput = "3, 8, 8, 3, 2, 8, 1, 2, 4, 56";
         HashMap<Integer, Integer> numberFrequencyChar = StringUtils.convertToHashMap(userInput);
         assertNotNull(numberFrequencyChar);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void exception_over_numbers_size() {
+        StringJoiner joiner = new StringJoiner(", ");
+        for (int index = 1; index <= 11; index++) {
+            joiner.add("3, 8, 8, 3, 2, 8, 1, 2, 4, 56");
+        }
+
+        String overSizeUserInput = joiner.toString();
+        StringUtils.convertToHashMap(overSizeUserInput);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void exception_under_numbers_size() {
+        String underSizeUserInput = "";
+        StringUtils.convertToHashMap(underSizeUserInput);
+    }
+
+    @Test(expected = NumberFormatException.class)
+    public void exception_not_number_input() {
+        String underSizeUserInput = "a, b, b, 1, 2, 3, 1, 2";
+        StringUtils.convertToHashMap(underSizeUserInput);
     }
 
     @Test
