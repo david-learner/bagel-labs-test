84495143ea68c6351703f0558a48cf12e4f466e5
David <xodnjs2549@naver.com>
2018-11-18 13:49:29 +0900

    add RomanNumeralsConverter

--- src/main/java/example03/RomanNumeralsConverter.java
+++ src/main/java/example03/RomanNumeralsConverter.java
@@ -1,10 +1,82 @@
 package example03;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class RomanNumeralsConverter {
-    public static String convert(int number) {
-        if (number == 0) {
-            return "";
+    private static Map<Integer, String> ROMAN_NUMERALS_CHART = new HashMap<Integer, String>() {
+        {
+            put(0, "");
+            put(1, "I");
+            put(2, "II");
+            put(3, "III");
+            put(4, "IV");
+            put(5, "V");
+            put(6, "VI");
+            put(7, "VII");
+            put(8, "VIII");
+            put(9, "IX");
+            put(10, "X");
+            put(50, "L");
+            put(100, "C");
         }
-        return "I";
+    };
+
+    public static String convert(String naturalNumber) {
+        String[] tokens = naturalNumber.split("");
+
+        if (tokens.length == 2) {
+            return convertTenth(tokens[0]) + "" + convertUnit(tokens[1]);
+        }
+
+        if (tokens.length == 3) {
+            return convertHundred(tokens[0]);
+        }
+
+        return convertUnit(tokens[0]);
+    }
+
+    public static String convertUnit(String unitNumber) {
+        return ROMAN_NUMERALS_CHART.get(Integer.parseInt(unitNumber));
+    }
+
+    public static String convertTenth(String tenthNumber) {
+        Integer ten = 10;
+        Integer fifty = 50;
+        Integer hundred = 100;
+        Integer tenth = Integer.parseInt(tenthNumber);
+        StringBuilder sb = new StringBuilder();
+
+        if (tenth == 4) {
+            return sb.append(ROMAN_NUMERALS_CHART.get(ten)).append(ROMAN_NUMERALS_CHART.get(fifty)).toString();
+        }
+
+        if (tenth == 5) {
+            return sb.append(ROMAN_NUMERALS_CHART.get(fifty)).toString();
+        }
+
+        if (tenth == 9) {
+            return sb.append(ROMAN_NUMERALS_CHART.get(ten)).append(ROMAN_NUMERALS_CHART.get(hundred)).toString();
+        }
+
+        if (tenth > 5) {
+            sb.append(ROMAN_NUMERALS_CHART.get(fifty));
+            return tenToThirty(sb, tenth - 5).toString();
+        }
+
+        return tenToThirty(sb, tenth).toString();
+    }
+
+    private static StringBuilder tenToThirty(StringBuilder sb, Integer tenth) {
+        for (int count = 0; count < tenth; count++) {
+            sb.append(ROMAN_NUMERALS_CHART.get(10));
+        }
+        return sb;
+    }
+
+
+    public static String convertHundred(String hundredNumber) {
+        Integer hundred = Integer.parseInt(hundredNumber) * 100;
+        return ROMAN_NUMERALS_CHART.get(hundred);
     }
 }
--- src/test/java/example03/RomanNumeralsConverterTest.java
+++ src/test/java/example03/RomanNumeralsConverterTest.java
@@ -3,21 +3,89 @@ package example03;
 import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertThat;
 
 public class RomanNumeralsConverterTest {
     @Test
     public void create() {
         RomanNumeralsConverter converter = new RomanNumeralsConverter();
+        assertNotNull(converter);
     }
 
     @Test
     public void convert() {
-        int[] zeroToTen = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-        String romanNumeral = RomanNumeralsConverter.convert(zeroToTen[0]);
-        assertThat(romanNumeral, is(""));
+        String romanNumber = RomanNumeralsConverter.convert("11");
+        assertThat(romanNumber, is("XI"));
+        romanNumber = RomanNumeralsConverter.convert("22");
+        assertThat(romanNumber, is("XXII"));
+        romanNumber = RomanNumeralsConverter.convert("33");
+        assertThat(romanNumber, is("XXXIII"));
+        romanNumber = RomanNumeralsConverter.convert("44");
+        assertThat(romanNumber, is("XLIV"));
+        romanNumber = RomanNumeralsConverter.convert("55");
+        assertThat(romanNumber, is("LV"));
+        romanNumber = RomanNumeralsConverter.convert("66");
+        assertThat(romanNumber, is("LXVI"));
+        romanNumber = RomanNumeralsConverter.convert("77");
+        assertThat(romanNumber, is("LXXVII"));
+        romanNumber = RomanNumeralsConverter.convert("88");
+        assertThat(romanNumber, is("LXXXVIII"));
+        romanNumber = RomanNumeralsConverter.convert("99");
+        assertThat(romanNumber, is("XCIX"));
+        romanNumber = RomanNumeralsConverter.convert("100");
+        assertThat(romanNumber, is("C"));
+    }
 
-        romanNumeral = RomanNumeralsConverter.convert(zeroToTen[1]);
-        assertThat(romanNumeral, is("I"));
+    @Test
+    public void convertUnit() {
+        String oneToTen = RomanNumeralsConverter.convertUnit("1");
+        assertThat(oneToTen, is("I"));
+        oneToTen = RomanNumeralsConverter.convertUnit("2");
+        assertThat(oneToTen, is("II"));
+        oneToTen = RomanNumeralsConverter.convertUnit("3");
+        assertThat(oneToTen, is("III"));
+        oneToTen = RomanNumeralsConverter.convertUnit("4");
+        assertThat(oneToTen, is("IV"));
+        oneToTen = RomanNumeralsConverter.convertUnit("5");
+        assertThat(oneToTen, is("V"));
+        oneToTen = RomanNumeralsConverter.convertUnit("6");
+        assertThat(oneToTen, is("VI"));
+        oneToTen = RomanNumeralsConverter.convertUnit("7");
+        assertThat(oneToTen, is("VII"));
+        oneToTen = RomanNumeralsConverter.convertUnit("8");
+        assertThat(oneToTen, is("VIII"));
+        oneToTen = RomanNumeralsConverter.convertUnit("9");
+        assertThat(oneToTen, is("IX"));
+        oneToTen = RomanNumeralsConverter.convertUnit("10");
+        assertThat(oneToTen, is("X"));
+    }
+
+    @Test
+    public void convertTenth() {
+        String tenth = RomanNumeralsConverter.convertTenth("1");
+        assertThat(tenth, is("X"));
+        tenth = RomanNumeralsConverter.convertTenth("2");
+        assertThat(tenth, is("XX"));
+        tenth = RomanNumeralsConverter.convertTenth("3");
+        assertThat(tenth, is("XXX"));
+        tenth = RomanNumeralsConverter.convertTenth("4");
+        assertThat(tenth, is("XL"));
+        tenth = RomanNumeralsConverter.convertTenth("5");
+        assertThat(tenth, is("L"));
+        tenth = RomanNumeralsConverter.convertTenth("6");
+        assertThat(tenth, is("LX"));
+        tenth = RomanNumeralsConverter.convertTenth("7");
+        assertThat(tenth, is("LXX"));
+        tenth = RomanNumeralsConverter.convertTenth("8");
+        assertThat(tenth, is("LXXX"));
+        tenth = RomanNumeralsConverter.convertTenth("9");
+        assertThat(tenth, is("XC"));
+    }
+
+    @Test
+    public void convertHundred() {
+        String hundred = RomanNumeralsConverter.convertHundred("1");
+        assertThat(hundred, is("C"));
     }
 }
